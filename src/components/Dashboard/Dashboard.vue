65667b02f02ef9234dc32095199f8e0349791090
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-15 22:40:17 +0545

    Dashboard component added

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-app id="inspire" light>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.title" @click="hello()">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    items: [
+      {title: 'Home', icon: 'dashboard'},
+      {title: 'About', icon: 'question_answer'}
+    ]
+  }),
+  props: {
+    source: String
+  }
+}
+</script>
