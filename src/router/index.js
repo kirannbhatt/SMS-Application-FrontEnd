66fd012c75c48696d5585f8c850b10bd0e9b4b5f
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-15 22:45:05 +0545

    components routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Dashboard from '@/components/Dashboard/Dashboard'
+import LogIn from '@/components/Login/Login'
+import SignUp from '@/components/SignUp/SignUp'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'HelloWorld',
+    component: Home
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LogIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  }
   ]
 })
