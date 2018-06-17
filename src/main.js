3109efc104198327c63e77a8a93e41d2d535fb4d
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-17 10:48:18 +0545

    v-toaster configured

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import Toaster from 'v-toaster'
+
 import 'vuetify/dist/vuetify.min.css'
+import 'v-toaster/dist/v-toaster.css'
 
 Vue.use(Vuetify)
+Vue.use(Toaster)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
