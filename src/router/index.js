f0ed432262d79f264567177d31b4f049889815ef
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-18 15:52:31 +0545

    Group and Contacts route added

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Auth from '../modules/Auth'
-import Home from '@/components/Home'
+import Main from '@/components/Main'
 import Dashboard from '@/components/Dashboard/Dashboard'
 import LogIn from '@/components/Login/Login'
 import SignUp from '@/components/SignUp/SignUp'
+import Home from '@/components/Dashboard/Home'
+import Groups from '@/components/Groups/Groups'
+import Contacts from '@/components/Contacts/Contacts'
 
 Vue.use(Router)
 
@@ -12,21 +15,8 @@ export default new Router({
   mode: 'history',
   routes: [{
     path: '/',
-    name: 'HelloWorld',
-    component: Home
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    beforeEnter: (to, from, next) => {
-      if (!Auth.isUserAuthenticated()) {
-        next({
-          path: '/login',
-          query: {'ua': true}
-        })
-      }
-    }
+    name: 'Main',
+    component: Main
   },
   {
     path: '/login',
@@ -45,6 +35,36 @@ export default new Router({
     path: '/signup',
     name: 'SignUp',
     component: SignUp
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    // beforeEnter: (to, from, next) => {
+    //   if (!Auth.isUserAuthenticated()) {
+    //     next({
+    //       path: '/login',
+    //       query: {'ua': true}
+    //     })
+    //   }
+    // },
+    children: [
+      {
+        path: 'groups',
+        name: 'Groups',
+        component: Groups
+      },
+      {
+        path: 'home',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: 'contacts',
+        name: 'Contacts',
+        component: Contacts
+      }
+    ]
   }
   ]
 })
