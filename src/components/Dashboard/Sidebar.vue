03852fdf68ff62c608a494ad30311e2ee8e5e49e
Kiran Bhatt <kbhatt7638@outlook.com>
2018-06-18 15:54:22 +0545

    Sidebar navigation seperated to another file

---
+++ src/components/Dashboard/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <v-list>
+            <v-list-tile to="/dashboard/home">
+            <v-list-tile-action>
+                <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content >
+                <v-list-tile-title>Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+        <v-list>
+            <v-list-tile to="/dashboard/groups">
+            <v-list-tile-action>
+                <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content >
+                <v-list-tile-title>Groups</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+        <v-list>
+            <v-list-tile to="/dashboard/contacts">
+            <v-list-tile-action>
+                <v-icon>dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content >
+                <v-list-tile-title>Contacts</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Sidebar'
+};
+</script>
+
